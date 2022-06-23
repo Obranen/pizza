b88bb3319bc14d38e7d383abc373ff607c82bb46
Obranen <krakhnytskyi@gmail.com>
2022-06-23 23:03:15 +0300

    styled logo.tsx

--- client/src/components/Header/Logo/Logo.tsx
+++ client/src/components/Header/Logo/Logo.tsx
@@ -6,7 +6,15 @@ const Logo = () => {
     return (
       <Col sm={4}>
         <div className={classes.logo}>
-          Logo
+            <div className={classes.word}>
+                <div className={classes.stripTop}></div>
+                <span className={classes.letterP}>P</span>
+                <span className={classes.letterI}>I</span>
+                <span className={classes.letterZ1}>Z</span>
+                <span className={classes.letterZ2}>Z</span>
+                <span className={classes.letterA}>A</span>
+                <div className={classes.stripBottom}></div>
+            </div>
         </div>
       </Col>
     )
