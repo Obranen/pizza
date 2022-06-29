8d83ab22179af4d0ff2a3a7c3507215678b76b52
Obranen <krakhnytskyi@gmail.com>
2022-06-29 23:32:22 +0300

    added navCircle for carousel

--- client/src/components/UI/CarouselUI/CarouselUI.tsx
+++ client/src/components/UI/CarouselUI/CarouselUI.tsx
@@ -2,53 +2,33 @@ import React, {useRef, useState} from 'react'
 import classes from './CarouselUI.module.scss'
 import {MdArrowBackIosNew, MdArrowForwardIos} from 'react-icons/md';
 import {Link} from 'react-router-dom';
-// import {ICarouselState} from '../../../store/types/carouselTypes';
-// import {useDispatchHook} from '../../../hooks/useDispatchHook';
 import {useSelectorHook} from '../../../hooks/useSelectorHook';
 
 const CarouselUI = () => {
-  // const {fetchCarousel} = useDispatchHook()
-  const {carousel, loading, error} = useSelectorHook(state => state.carouselReducer)
+  const {carousel} = useSelectorHook(state => state.carouselReducer)
   const itemList = useRef<any>(null)
-  const [currentLengthList, setCurrentLengthList] = useState<number | string>(0)
+  const [currentLengthList, setCurrentLengthList] = useState<number>(0)
 
-  if (loading) {
-    return <h1>Идёт загрузка...</h1>
+  const lengthCarousel = () => {
+    const lengthItem = +getComputedStyle(itemList.current).width.slice(0, -2)
+    const totalLength = lengthItem * +(carousel.length)
+    return {lengthItem, totalLength}
   }
-  if (error) {
-    return <h1>{error}</h1>
-  }
-
-  // useEffect(() => {
-  //   fetchCarousel(items)
-  // }, [])
-
-  //Добавление
-  // const optionsSlider = (optionsSlider: any) => {
-  //   setItems([...items, {
-  //     imageSrc: optionsSlider.imageSrc,
-  //     imageAlt: optionsSlider.imageAlt,
-  //     linkUrl: optionsSlider.linkUrl
-  //   }])
-  // }
-  // optionsSlider({imageSrc: 'фотка 4', imageAlt: 'фотка4', linkUrl: '/'})
 
   const arrowLeftHandler = () => {
-    const LengthItem = getComputedStyle(itemList.current).width.slice(0, -2)
-    const totalLength = +LengthItem * (carousel.length)
+    const {lengthItem, totalLength} = lengthCarousel()
     if (currentLengthList < 0) {
-      const currentLengthItem = +currentLengthList + +LengthItem
+      const currentLengthItem = currentLengthList + lengthItem
       setCurrentLengthList(currentLengthItem)
     } else {
-      const currentLengthItem = +totalLength - +LengthItem
+      const currentLengthItem = +totalLength - +lengthItem
       setCurrentLengthList(-currentLengthItem)
     }
   }
 
   const arrowRightHandler = () => {
-    const LengthItem = getComputedStyle(itemList.current).width.slice(0, -2)
-    const totalLength = +LengthItem * (carousel.length)
-    const currentLengthItem = +LengthItem - +currentLengthList
+    const {lengthItem, totalLength} = lengthCarousel()
+    const currentLengthItem = +lengthItem - +currentLengthList
     if (currentLengthItem !== totalLength) {
       setCurrentLengthList(-currentLengthItem)
     } else {
@@ -56,24 +36,34 @@ const CarouselUI = () => {
     }
   }
 
+  const circleNavHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    const {lengthItem} = lengthCarousel()
+    const numberCircle = event.currentTarget.getAttribute('data-number')
+    const currentLengthItem = lengthItem * Number(numberCircle)
+    setCurrentLengthList(-currentLengthItem)
+  }
+
   return (
-    <>
-      <div className={classes.slider}>
-        <MdArrowBackIosNew className={classes.arrowLeft} onClick={arrowLeftHandler}/>
-        <div className={classes.visibleItem}>
-          <div ref={itemList} style={{transform: `translateX(${currentLengthList}px)`}} className={classes.list}>
-            {carousel.map((item, index) =>
-              <div key={index}>
-                <Link to={item.linkUrl}>
-                  <img src={item.imageSrc} alt={item.imageAlt}/>
-                </Link>
-              </div>
-            )}
-          </div>
+    <div className={classes.slider}>
+      <MdArrowBackIosNew className={classes.arrowLeft} onClick={arrowLeftHandler}/>
+      <div className={classes.visibleItem}>
+        <div ref={itemList} style={{transform: `translateX(${currentLengthList}px)`}} className={classes.list}>
+          {carousel.map((item, index) =>
+            <div key={index}>
+              <Link to={item.linkUrl}>
+                <img src={item.imageSrc} alt={item.imageAlt}/>
+              </Link>
+            </div>
+          )}
         </div>
-        <MdArrowForwardIos onClick={arrowRightHandler} className={classes.arrowRight}/>
       </div>
-    </>
+      <div className={classes.circleWrapper}>
+        {carousel.map((item, index) =>
+          <div key={index} data-number={index} onClick={circleNavHandler} className={classes.circle}></div>
+        )}
+      </div>
+      <MdArrowForwardIos onClick={arrowRightHandler} className={classes.arrowRight}/>
+    </div>
   )
 }
 
